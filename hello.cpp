19b136f4285c09254884e403e7383fde9956393b
Limitless741 <92598649+Limitless741@users.noreply.github.com>
2023-02-17 18:09:23 +0530

    Delete hello.cpp

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, I am Adarsha of SEC-I\n";
-  cout << "I am in Task5 and my SRN is PES1UG20CS544\n";
-  cout << "Resolved the error";
-  return 0;
-}
